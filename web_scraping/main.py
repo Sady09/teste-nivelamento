fa84b6a3f98d421d54ebee7555ebc00f5b8c35c9
Sady <joaosady667@gmail.com>
2025-03-26 14:25:31 -0300

    fix: corrige criação e uso do diretório downloads para salvar arquivos

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -1,8 +1,13 @@
 import requests
 import zipfile
+import os
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos')
+url = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
+
+os.makedirs('./downloads', exist_ok=True)
+
+r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
 anexo_1 = soup.find('a', href=True, string='Anexo I.')
@@ -15,8 +20,8 @@ if anexo_1 and anexo_2:
     anexo1_response = requests.get(anexo1_url)
     anexo2_response = requests.get(anexo2_url)
 
-    anexo1_filename = 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf'
-    anexo2_filename = 'Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf'
+    anexo1_filename = os.path.join('./downloads', 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')
+    anexo2_filename = os.path.join('./downloads', 'Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf')
 
     with open(anexo1_filename, 'wb') as f1:
         f1.write(anexo1_response.content)
@@ -26,9 +31,9 @@ if anexo_1 and anexo_2:
         f2.write(anexo2_response.content)
     print('Anexo II baixado com sucesso!')
 
-    with zipfile.ZipFile('anexos.zip', 'w') as zipf:
-        zipf.write(anexo1_filename)
-        zipf.write(anexo2_filename)
+    with zipfile.ZipFile(os.path.join('./downloads', 'anexos.zip'), 'w') as zipf:
+        zipf.write(anexo1_filename, 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')
+        zipf.write(anexo2_filename, 'Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf')
     print('Arquivos compactados com sucesso em anexos.zip!')
 else:
     if not anexo_1:
