de5ed0ea3ceda3272c9f2b2d24e275ff37f5c24c
Sady <joaosady667@gmail.com>
2025-03-26 13:36:47 -0300

    feat: implementa compactação dos arquivos pdf

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -1,4 +1,5 @@
 import requests
+import zipfile
 from bs4 import BeautifulSoup
 
 r = requests.get('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos')
@@ -14,13 +15,21 @@ if anexo_1 and anexo_2:
     anexo1_response = requests.get(anexo1_url)
     anexo2_response = requests.get(anexo2_url)
 
-    with open('Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf', 'wb') as f1:
+    anexo1_filename = 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf'
+    anexo2_filename = 'Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf'
+
+    with open(anexo1_filename, 'wb') as f1:
         f1.write(anexo1_response.content)
     print('Anexo I baixado com sucesso!')
 
-    with open('Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf', 'wb') as f2:
+    with open(anexo2_filename, 'wb') as f2:
         f2.write(anexo2_response.content)
     print('Anexo II baixado com sucesso!')
+
+    with zipfile.ZipFile('anexos.zip', 'w') as zipf:
+        zipf.write(anexo1_filename)
+        zipf.write(anexo2_filename)
+    print('Arquivos compactados com sucesso em anexos.zip!')
 else:
     if not anexo_1:
         print('Anexo I não encontrado!')
